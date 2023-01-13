5d13911c4f43e6bde784bebdf3e95703c24e7370
Ashrib <asharib920@gmail.com>
2023-01-13 19:05:00 +0500

    some updates

--- product/src/components/home/home.jsx
+++ product/src/components/home/home.jsx
@@ -242,7 +242,7 @@ function Home() {
   return (
   <div class="container-fluid">
     <div className="side-nav">
-      <div onClick={logoutHandler}>Logout</div>
+      <div onClick={logoutHandler}>logout</div>
     
     </div>
     <div className="center-body">
